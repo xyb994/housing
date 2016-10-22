5e99ca03ee94ecfc747e15d32ffff472194de034
Yibo <xyb994@gmail.com>
2016-10-22 04:34:31 -0400

    Add index and detail to views in listings

--- housing/listings/views.py
+++ housing/listings/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from .models import Listing
 
 # Create your views here.
+def index(request):
+    latest_listings = Listing.objects.order_by('-datetime_modified')[:10]
+    context = {'latest_listings': latest_listings}
+    return render(request, 'listings/index.html', context)
+
+def detail(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    return render(request, 'listings/detail.html', {'listing': listing})
