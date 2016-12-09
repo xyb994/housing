2ee65d1f6689b17173139ba1d0a5b8414786cecc
Yibo <xyb994@gmail.com>
2016-12-09 05:29:58 -0500

    Fix pagination with filtered result

--- housing/listings/views.py
+++ housing/listings/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import FilterForm
 
-from . import forms
 from .models import Listing
 
 # Create your views here.
 def index(request):
+    queries_without_page = request.GET.copy()
+
+    if "page" in queries_without_page:
+        del queries_without_page["page"]
+
     filter_querry = {"is_active": True}
+
+    if (request.GET.get("price")):
+        filter_querry["lease_monthly_cost__gte"] = request.GET.get("price")
 
     if (request.GET.get("type") in ["house", "apartment", "homestay"]):
         filter_querry["property_type"] = request.GET.get("type")
 
     if (request.GET.get("beds")):
         filter_querry["bedroom_count__gte"] = request.GET.get("beds")
-
-    if (request.GET.get("price")):
-        filter_querry["lease_monthly_cost__gte"] = request.GET.get("price")
 
     listings = Listing.objects.filter(**filter_querry).order_by("-datetime_modified")
 
@@ -30,7 +35,7 @@ def index(request):
     except EmptyPage:
         listings = paginator.page(1)
 
-    return render(request, "listings/index.html", {"listings": listings })
+    return render(request, "listings/index.html", {"listings": listings, 'queries_without_page': queries_without_page})
 
 def detail(request, listing_id):
     listing = get_object_or_404(Listing, pk=listing_id)
