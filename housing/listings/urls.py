975b6531401accbe2b878aacc5af36621ce7b371
Yibo <xyb994@gmail.com>
2016-12-12 04:02:50 -0500

    Update newListing to urls.py

--- housing/listings/urls.py
+++ housing/listings/urls.py
@@ -7,4 +7,5 @@ app_name = 'listings'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<listing_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^newListing$', views.newListing, name='newListing'),
 ]
