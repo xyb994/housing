15f1ec3c608be5cc2c3ba7143a6fcfac151933d5
Yibo <xyb994@gmail.com>
2016-12-12 13:06:12 -0500

    Update urls

--- housing/listings/urls.py
+++ housing/listings/urls.py
@@ -8,8 +8,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<listing_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^newListing$', views.newListing, name='newListing'),
-<<<<<<< Updated upstream
-=======
     url(r'^accounts/', include('nopassword.urls', namespace='nopassword')),
->>>>>>> Stashed changes
 ]
