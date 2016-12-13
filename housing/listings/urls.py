98227b75578b5f244d2684ef05d92650ae0a17b6
Yibo <xyb994@gmail.com>
2016-12-12 23:14:55 -0500

    Add registration urls

--- housing/listings/urls.py
+++ housing/listings/urls.py
@@ -7,5 +7,7 @@ app_name = 'listings'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<listing_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^accounts/register/$', views.register, name='register'),
+    url(r'^accounts/register/complete/$', views.registration_complete, name='registration_complete'),
     url(r'^newListing$', views.newListing, name='newListing'),
 ]
