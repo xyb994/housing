5f60befacf697b0b6c6585710c260a2f9cbd204d
Yibo <xyb994@gmail.com>
2016-12-14 16:16:42 -0500

    Remove Images model import

--- housing/listings/forms.py
+++ housing/listings/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Listing, HousingUser, Images
+from .models import Listing, HousingUser
 
 class HousingUserCreationForm(forms.ModelForm):
     class Meta:
