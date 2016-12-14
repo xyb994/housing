664eb29d3763ef751ff350601cab9635a2bbeaf3
Yibo <xyb994@gmail.com>
2016-12-14 07:46:07 -0500

    Update views, use generic view class

--- housing/listings/views.py
+++ housing/listings/views.py
@@ -1,36 +1,18 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.shortcuts import redirect, render, render_to_response
+from django.views import generic
+from django.urls import reverse_lazy
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect
 from django.template.context_processors import csrf
-from datetime import datetime
 
 from .forms import ListingForm, HousingUserCreationForm
 from .models import Listing
 
-# index homepage view
+# index View
 def index(request):
-    queries_without_page = request.GET.copy()
-    if "page" in queries_without_page:
-        del queries_without_page["page"]
-
-    filter_querry = {"is_active": True}
-
-    if (request.GET.get("price")):
-        filter_querry["lease_monthly_cost__gte"] = request.GET.get("price")
-
-    if (request.GET.get("type") in ["house", "apartment", "homestay"]):
-        filter_querry["property_type"] = request.GET.get("type")
-
-    if (request.GET.get("beds")):
-        filter_querry["bedroom_count__gte"] = request.GET.get("beds")
-
-    listings = Listing.objects.filter(**filter_querry).order_by(
+    listings = Listing.objects.filter(**get_queryset(request)).order_by(
         "-datetime_modified")
 
+    # pagination
     paginator = Paginator(listings, 5) #listing per page
     page = request.GET.get("page")
 
@@ -41,26 +23,49 @@ def index(request):
     except EmptyPage:
         listings = paginator.page(1)
 
-    return render(request, "listings/index.html", {"listings": listings,
-        "queries_without_page": queries_without_page,
-        "filter_querry": filter_querry})
+    # store queryset, pass for pagination
+    queries_without_page = request.GET.copy()
+    if "page" in queries_without_page:
+        del queries_without_page["page"]
 
-# listing detail view
-def detail(request, listing_id):
-    listing = get_object_or_404(Listing, pk=listing_id)
-    return render(request, "listings/detail.html", {"listing": listing})
+    return render(request, "listings/index.html",
+        {"listings": listings, "queries_without_page": queries_without_page})
+
+
+# get querysets using GET
+def get_queryset(request):
+    queryset = {"is_active": True}
+
+    if request.GET.get("price"):
+        queryset["lease_monthly_cost__gte"] = request.GET.get("price")
+
+    if request.GET.get("type") in ["house", "apartment", "homestay"]:
+        queryset["property_type"] = request.GET.get("type")
+
+    if request.GET.get("beds"):
+        queryset["bedroom_count__gte"] = request.GET.get("beds")
+
+    return queryset
+
+
+class ListingDetail(generic.DetailView):
+    model = Listing
+    template_name = "listings/detail.html"
+    context_object_name = "listing"
+    pk_url_kwarg = "listing_id"
+
 
 # registration view
 def register(request):
     if request.method == "POST":
         form = HousingUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            user = form.save()
             user.datetime_created = datetime.now()
             user.email = user.username
             user.set_password(request.POST.get("password"))
-            user.save()
-            return HttpResponseRedirect("/accounts/register/complete")
+            form.save()
+            return redirect("/accounts/register/complete")
     # if a GET (or any other method), create a blank form
     else:
         form = HousingUserCreationForm()
@@ -68,60 +73,83 @@ def register(request):
     token.update(csrf(request))
     token["form"] = form
 
-    return render_to_response("registration/registration_form.html", token)
+    return render_to_response("accounts/registration_form.html", token)
 
-# registration complete view
-def registration_complete(request):
-    return render_to_response("registration/registration_complete.html")
 
-# account profile view
-@login_required
-def profile(request):
-    if request.method == "POST":
-        listing_id = request.POST.get("listing-to-be-changed")
-        print("listing id:")
-        print(listing_id)
+class RegistrationCompleteView(generic.TemplateView):
+    template_name = "accounts/registration_complete.html"
+
+
+class ProfileView(generic.ListView):
+    model = Listing
+    template_name = "accounts/profile.html"
+    context_object_name = "listings"
+
+    # listings belong to the user
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ProfileView, self).get_context_data(**kwargs)
+
+        current_user = self.request.user
+        context['listings'] = Listing.objects.filter(
+            listing_owner=current_user).order_by(
+            "-is_active", "-datetime_created")
+
+        return context
+
+
+class ListingCreate(generic.CreateView):
+    model = Listing
+    form_class = ListingForm
+    template_name = "listings/listing_edit.html"
+    success_url = reverse_lazy("listings:profile")
+
+    def form_valid(self, form):
+        form.instance.listing_owner = self.request.user
+
+        return super(ListingCreate, self).form_valid(form)
+
+
+class ListingEdit(generic.UpdateView):
+    model = Listing
+    form_class = ListingForm
+    template_name = "listings/listing_edit.html"
+    pk_url_kwarg = "listing_id"
+    success_url = reverse_lazy("listings:profile")
+
+
+def listing_status_toggle(request, listing_id):
         listing = Listing.objects.get(pk=listing_id)
 
-        if (listing.is_active):
+        if listing.is_active:
             listing.is_active = False
         else:
             listing.is_active = True
 
         listing.save()
 
-    listings = Listing.objects.filter(listing_owner=request.user).order_by(
-        "-is_active", "-datetime_created")
-    return render(request, "accounts/profile.html", {"listings": listings})
+        return redirect("/accounts/profile")
 
-# Create new listing view
-@login_required
-def newListing(request):
-    if request.method == "POST":
-        form = ListingForm(request.POST)
-        if form.is_valid():
-            listing = form.save(commit=False)
-            listing.datetime_modified = datetime.now()
-            listing.listing_owner = request.user
-            listing.save()
-            return redirect("/accounts/profile")
-    # if a GET (or any other method), create a blank form
-    else:
-        form = ListingForm()
 
-    return render(request, "listings/listing_edit.html", {"form": form})
-
-# Edit Listing Detail View
-def editListing(request, pk):
-    listing = get_object_or_404(Listing, pk=pk)
-    if request.method == "POST":
-        form = ListingForm(request.POST, instance=post)
-        if form.is_valid():
-            listing = form.save(commit=False)
-            listing.author = request.user
-            listing.published_date = timezone.now()
-            listing.save()
-            return redirect('post_detail', pk=post.pk)
-    else:
-        form = ListingForm(instance=post)
-    return render(request, 'listings/listing_edit.html', {'form': form})
+# class IndexView(generic.list.ListView):
+#     template = "listings/index.html"
+#     context_object_name = "listings"
+#     paginate_by = 5
+#http://stackoverflow.com/questions/5907575/how-do-i-use-pagination-with-django-class-based-generic-listviews
+#
+#     def get_queryset(self):
+#         queryset = {"is_active": True}
+#
+#         if (request.GET.get("price")):
+#             queryset["lease_monthly_cost__gte"] = request.GET.get("price")
+#
+#         if (request.GET.get("type") in ["house", "apartment", "homestay"]):
+#             queryset["property_type"] = request.GET.get("type")
+#
+#         if (request.GET.get("beds")):
+#             queryset["bedroom_count__gte"] = request.GET.get("beds")
+#
+#         listings = Listing.objects.filter(**filter_querry).order_by(
+#             "-datetime_modified")
+#
+#         return listings
