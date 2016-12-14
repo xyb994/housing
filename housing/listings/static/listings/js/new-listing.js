3902fcc4beb6abfa69bd263717a24acdf3ed17fb
Yibo <xyb994@gmail.com>
2016-12-14 07:01:05 -0500

    Update javascript to clear input field after hide

--- housing/listings/static/listings/js/new-listing.js
+++ housing/listings/static/listings/js/new-listing.js
@@ -1,24 +1,31 @@
 'use strict'
 
 $(document).ready(function(){
-  $('#lease_duration_custom').hide();
-  $('#furnished_details').hide();
+  if ($('#id_furnished').val() == 'lightly_furnished' || $('#id_lease_duration').val() == 'fully_furnished') {
+    $('#furnished_details').show('fast');
+  }
 
-  $("#id_lease_duration").change(function() {
+  if ($('#id_lease_duration').val() != 'other') {
+    $('#lease_duration_custom').hide('fast');
+  }
+
+  $('#id_lease_duration').change(function() {
     let lease_duration = $(this).val();
-    if (lease_duration == "other") {
-      $("#lease_duration_custom").show("fast");
+    if (lease_duration == 'other') {
+      $('#lease_duration_custom').show('fast');
     } else {
-      $("#lease_duration_custom").hide("fast");
+      $('#lease_duration_custom').hide('fast');
+      $('#id_lease_duration_custom').val("");
     }
   })
 
-  $("#id_furnished").change(function() {
+  $('#id_furnished').change(function() {
     let furnished = $(this).val();
-    if (furnished == "lightly_furnished" || furnished == "fully_furnished") {
-      $("#furnished_details").show("fast");
+    if (furnished == 'lightly_furnished' || furnished == 'fully_furnished') {
+      $('#furnished_details').show('fast');
     } else {
-      $("#furnished_details").hide("fast");
+      $('#furnished_details').hide('fast');
+      $('#id_furnished_details').val("");
     }
   })
 })
