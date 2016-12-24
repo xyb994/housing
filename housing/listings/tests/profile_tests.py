c316ef7c7851d19ce9a4784edd029bc2f592164d
Yibo <xyb994@gmail.com>
2016-12-24 12:30:18 -0500

    Update profile_tests

--- housing/listings/tests/profile_tests.py
+++ housing/listings/tests/profile_tests.py
@@ -9,18 +9,14 @@ class ProfileTest(TestCase):
         self.user = factories.HousingUserFactory()
         self.listings = factories.ListingFactory.create_batch(listing_owner=self.user, size=5)
 
-        client.force_login(user)
+        client.force_login(self.user)
 
         self.profile_url = reverse('listings:profile')
         self.response = client.get(self.profile_url)
 
 
     def get_edit_listing_url(self, listing):
-        return reverse('listings:edit', kwargs={"pk": listing.pk})
-
-    def test_get_listings(self):
-        for listing in self.user.get_listings:
-            self.assertIn(listing, self.listings)
+        return reverse('listings:edit', kwargs={"listing_id": listing.pk})
 
     def test_return_status_is_successful(self):
         self.assertEquals(self.response.status_code, 200)
